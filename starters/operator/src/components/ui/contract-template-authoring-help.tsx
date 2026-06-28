458cf04bb7459c2fbba0bedc394701819cc6eabc
Mihai Ungureanu <92330332+mihaipxm@users.noreply.github.com>
2026-06-28 09:51:39 +0300

    Remove unused operator UI shims (#2309)

--- starters/operator/src/components/ui/contract-template-authoring-help.tsx
+++
@@ -1,6 +0,0 @@
-export {
-  ContractTemplateAuthoringHelp,
-  type TemplateAuthoringSnippet,
-  type TemplateAuthoringVariable,
-  type TemplateAuthoringVariableGroup,
-} from "@voyant-travel/ui/components/contract-template-authoring-help"
