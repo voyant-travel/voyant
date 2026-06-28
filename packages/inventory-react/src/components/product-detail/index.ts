cc25e7789b8386ec88004697da05ae376a41d5e6
Mihai Ungureanu <mihai@pixelmakers.com>
2026-06-28 12:36:59 +0300

    Preserve product detail query helper aliases

--- packages/inventory-react/src/components/product-detail/index.ts
+++ packages/inventory-react/src/components/product-detail/index.ts
@@ -11,12 +11,15 @@ export { ProductDetailPage } from "./product-detail-page.js"
 export {
   getChannelsQueryOptions,
   getProductChannelMappingsQueryOptions,
+  getProductDetailMediaQueryOptions as getProductMediaQueryOptions,
   getProductDetailMediaQueryOptions,
   getProductRulesQueryOptions,
   getProductSlotsQueryOptions,
 } from "./product-detail-shared.js"
 export { ProductDetailSkeleton } from "./product-detail-skeleton.js"
 export {
+  getProductDetailPricingCategoriesQueryOptions as getPricingCategoriesQueryOptions,
   getProductDetailPricingCategoriesQueryOptions,
+  getProductDetailProductOptionsQueryOptions as getProductOptionsQueryOptions,
   getProductDetailProductOptionsQueryOptions,
 } from "./product-options-shared.js"
